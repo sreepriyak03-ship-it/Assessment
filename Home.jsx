317885f4ae676d367d6a64462c743a63a34738ee
sreepriyak03-ship-it <sreepriyak03@gmail.com>
2025-11-29 10:36:31 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+const home=()=>{
+    return (
+    <div className="flex items-center justify-center">
+        <h1 className="text-3xl font-semibold text-pink-900">Welcome to my Home Page </h1>
+     </div>
+)
+
+}
+
+export default home
